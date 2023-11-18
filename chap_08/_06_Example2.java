1583c9b6be839d8835e15f35aea653763a8c2a70
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-19 00:06:15 +0900

    Upload _06_Example2.java

---
+++ chap_08/_06_Example2.java
@@ -0,0 +1,25 @@
+package chap_08;
+
+public class _06_Example2 {
+    public static void main(String[] args) {
+        System.out.print(1 + " / ");
+        try {
+            System.out.print(2 + " / ");
+            // 1) ArithmeticException 발생
+            System.out.print(0 / 0);
+            System.out.print(3 + " / ");
+        // 2) catch 블럭 확인1
+        } catch (ClassCastException ce) { // if (ce instanceof ArithmeticException) -> false
+            System.out.print(4 + " ");
+        // 3) catch 블럭 확인2 -> 예외 처리
+        } catch (Exception e) {
+            if (e instanceof ArithmeticException) // true
+                System.out.print("e instanceof ArithmeticException -> true" + " / ");
+            System.out.print(5 + " / ");
+        }
+        System.out.println("프로그램 정상 종료");
+        /*
+        1 / 2 / e instanceof ArithmeticException -> true / 5 / 프로그램 정상 종료
+        */
+    }
+}
