061f426e6e4f8c95b4444ff78d1bcaf1b32a3836
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-24 16:50:50 +0900

    Update _16_Example2.java

--- chap_08/_16_Example2.java
+++ chap_08/_16_Example2.java
@@ -1,8 +1,8 @@
-// 2) RuntimeException 클래스를 상속받는 사용자 정의 예외 클래스를 정의하는 경우
+// 2) RuntimeException 클래스를 상속받는 예외 클래스를 정의하는 경우
 
 package chap_08;
 
-// 2-1) 사용자 정의 예외 클래스 정의
+// 2-1) 예외 클래스 정의
 class MyException2 extends RuntimeException {
     private final int ERR_CODE;
 
