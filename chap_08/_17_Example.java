19f9918932d5843d477dc74283bac7c29756da67
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-26 00:22:15 +0900

    Update _17_Example.java

--- chap_08/_17_Example.java
+++ chap_08/_17_Example.java
@@ -6,10 +6,9 @@ public class _17_Example {
     public static void main(String[] args) {
         try {
             createFile();
+            // 2-2) 두 번째 예외 처리
         } catch (Exception e) {
-            // 2-2) 두 번째 예외 처리 완료
             System.out.println("메시지: " + e.getMessage());
-            System.out.println("두 번째 예외 처리 완료");
         }
     }
 
@@ -17,10 +16,9 @@ public class _17_Example {
         try {
             // 1-1) 첫 번째 예외 발생
             throw new Exception("첫 번째 예외 발생");
-        // 1-2) 첫 번째 예외 처리 완료
+            // 1-2) 첫 번째 예외 처리
         } catch (Exception e) {
             System.out.println("메시지: " + e.getMessage());
-            System.out.println("첫 번째 예외 처리 완료");
             // 2-1) 예외 되던지기로 두 번째 예외 발생
             throw new Exception("두 번째 예외 발생");
         }
@@ -29,7 +27,5 @@ public class _17_Example {
 
 /*
 메시지: 첫 번째 예외 발생
-첫 번째 예외 처리 완료
 메시지: 두 번째 예외 발생
-두 번째 예외 처리 완료
 */
