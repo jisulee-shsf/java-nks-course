8b29367b47c778a4be682dcb63a4dc9cb43845ab
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-27 03:41:19 +0900

    Update _03_Example.java

--- chap_09/_03_Example.java
+++ chap_09/_03_Example.java
@@ -29,7 +29,7 @@ class ExampleClass2 {
 
 public class _03_Example {
     public static void main(String[] args) {
-        // 2-1) 두 객체의 인스턴스 변숫값이 서로 같을 때 equals 메서드로 비교할 경우
+        // 2-1) 두 객체의 인스턴스 변숫값이 서로 같을 때 오버라이딩한 equals 메서드로 비교할 경우
         ExampleClass2 e1 = new ExampleClass2(100);
         ExampleClass2 e2 = new ExampleClass2(100);
         System.out.println("e1.equals(e2): "+ e1.equals(e2));
@@ -41,7 +41,7 @@ public class _03_Example {
         e1.toString(): chap_09.ExampleClass2@75b84c92 / e2.toString(): chap_09.ExampleClass2@6bc7c054
         */
 
-        // 2-2) 두 객체의 인스턴스 변숫값이 서로 다를 때 equals 메서드로 비교할 경우
+        // 2-2) 두 객체의 인스턴스 변숫값이 서로 다를 때 오버라이딩한 equals 메서드로 비교할 경우
         e2.value = 200;
         System.out.println("e1.equals(e2): "+ e1.equals(e2));
         System.out.println("e1.value: " + e1.value + " / e2.value: " + e2.value);
