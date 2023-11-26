6c9ac2f513e98ce10853404b51723676eefa62ae
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-27 02:29:04 +0900

    Upload _06_Example2.java

---
+++ chap_09/_06_Example2.java
@@ -0,0 +1,22 @@
+package chap_09;
+
+class ExampleClass5 {
+    int value;
+
+    ExampleClass5(int value) {
+        this.value = value;
+    }
+
+    // 1) toString 메서드 오버라이딩
+    public String toString() {
+        return "value = " + value;
+    }
+}
+
+public class _06_Example2 {
+    public static void main(String[] args) {
+        // 2) 인스턴스 변수값을 문자열로 변환해 반환한 경우
+        ExampleClass5 e = new ExampleClass5(100);
+        System.out.println("e.toString(): " + e); // e.toString(): value = 100
+    }
+}
