108b285a583b43d748368d3ee66e06c4cb1180b7
Jisu Lee <jisulee.shsf@gmail.com>
2023-12-19 23:45:02 +0900

    Update _18_Example.java

--- chap_11/_18_Example.java
+++ chap_11/_18_Example.java
@@ -1,9 +1,12 @@
 package chap_11;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class _18_Example {
     public static void main(String[] args) {
+        // 1) Stack 클래스로 구현한 Stack
         Stack stack = new Stack();
         stack.push("0");
         stack.push("1");
@@ -14,6 +17,7 @@ public class _18_Example {
         }
         System.out.println();
 
+        // 2) Queue 인터페이스를 구현한 LinkedList 클래스로  Queue
         Queue queue = new LinkedList();
         queue.offer("0");
         queue.offer("1");
